647751afb8875990bad4bffe4608b5258fc74a15
JoshuaAlvarado23 <40558240+JoshuaAlvarado23@users.noreply.github.com>
2019-02-03 01:24:18 +0800

    New UI directory

---
+++ TargetUI/auth.php
@@ -0,0 +1,51 @@
+
+
+<html>
+<head>
+
+	<title>Admin Login</title>
+	<meta lang="en-US">
+	<meta charset="utf-8">
+	<meta content="width=device-width, initial-scale=1.0" name="viewport">
+	<meta content="" name="keywords">
+	<meta content="" name="description">
+
+	<link rel="stylesheet" href="./css/animate/animate.css">
+	<link href="./css/bootstrap/bootstrap.min.css" type="text/css" rel="stylesheet">
+	<link href="./css/fontawesome/css/all.min.css" type="text/css" rel="stylesheet">
+	<link href="./css/style.css" type="text/css" rel="stylesheet">
+
+	<style type="text/css">
+		body {
+			background-color: #fff9d1;
+		}
+
+		.instyle {
+			margin: 10px;
+		}
+
+		input[submit] {
+			background-color: #B4710A
+		}
+	</style>
+
+</head>
+
+<body>
+	<div class="container-fluid centering text-center" style="background-color: #B4710A; padding: 50px; border-radius: 3px">
+	<div>
+		<div class="form-group instyle" > 
+			<div>
+				<input style="margin-bottom: 10px; border-radius: 5px" class="text-center"type="text" name="username" placeholder="Username">
+			</div>
+			<div>
+				<input style="margin-bottom: 10px; border-radius: 5px"  type="password" name="password" placeholder="Password">
+			</div>
+			<div>
+				<button class="btn btn-primary btn-block cust-btn" type="submit">??????</button>
+			</div>
+		</div>		
+	</div>
+	</div>
+</body>
+</html>
